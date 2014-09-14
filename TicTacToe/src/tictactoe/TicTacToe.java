358d62ce1531b34ba7b311067b80a23b1589a21c
ZerefeL <zerefel@gmail.com>
2014-09-14 14:10:15 +0300

    removed some uneeded code

--- TicTacToe/src/tictactoe/TicTacToe.java
+++ TicTacToe/src/tictactoe/TicTacToe.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package tictactoe;
 
 import javafx.application.Application;
@@ -16,10 +10,7 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-/**
- *
- * @author ZerefeL
- */
+
 public class TicTacToe extends Application {
     
     @Override
